feadc341776398b152b2839809326a87d8f9bf75
Jason Chow <jchow149@my.bcit.ca>
2023-10-31 17:20:27 -0700

    Added own api

--- scripts/firebaseAPI_DTC_09.js.js
+++ scripts/firebaseAPI_DTC_09.js.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyB57xi50lvxIl4VAk0yT_wT2V4NCSKcnSQ",
+    authDomain: "infolot-6f879.firebaseapp.com",
+    projectId: "infolot-6f879",
+    storageBucket: "infolot-6f879.appspot.com",
+    messagingSenderId: "1026901899909",
+    appId: "1:1026901899909:web:23d6e578b5acb354ae07ec"
 };
 
 //--------------------------------------------
