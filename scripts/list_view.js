bfd9383fb7facc1e2b85167ceb61247e1eedc772
FRANCESCA FUENTES <francescafuentesxo@gmail.com>
2023-11-04 16:56:24 -0700

    Added 2 parking spots to the list view page

---
+++ scripts/list_view.js
@@ -0,0 +1,31 @@
+function displayCardsDynamically(collection) {
+    let cardTemplate = document.getElementById("parkingLotsCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
+
+    db.collection(collection).get()   //the collection called "hikes"
+        .then(allLots => {
+            //var i = 1;  //Optional: if you want to have a unique ID for each hike
+            allLots.forEach(doc => { //iterate thru each doc
+                var title = doc.data().name;       // get value of the "name" key
+                var details = doc.data().details;  // get value of the "details" key
+                var parkingCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
+                let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('.card-image').src = `./lot_images/${parkingCode}.jpg`; //Example: NV01.jpg
+
+                //Optional: give unique ids to all elements for future use
+                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                //attach to gallery, Example: "hikes-go-here"
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+
+                //i++;   //Optional: iterate variable to serve as unique ID
+            })
+        })
+}
+
+displayCardsDynamically("parkingLots");  //input param is the name of the collection
