173c844d193a56d58cb5546991d2dee6ca6b6d59
Jason Chow <jchow149@my.bcit.ca>
2023-11-07 16:48:08 -0800

    Temp JS file

---
+++ scripts/parking_info2.js
@@ -0,0 +1,97 @@
+parkingRef.add({
+    code: "TELUSGardens9174",
+    name: "TELUS Gardens Residences - Parking Lot #9174",
+    city: "Vancouver",
+    province: "BC",
+    address: "783 Richards St, Vancouver, BC V6B 0M6",
+    hours_of_operation: "6am-11pm",
+    rate: "$9.00 per hour",
+    details: "Vancouver's newest parkade! A beautiful 50 stall underground parkade at the corner of Robson and Richards Street. The parkade is a pay by license plate operation that has paystations at each elevator lobby for immediate convenience. All you need to do is park, pay at the paystation with your license plate and be on your way to your destination. Electric vehicles parked in the stalls with chargers must pay Chargepoint for the power usage and at the meter for the parking.",
+    lat: 49.28089214915153,
+    lng: -123.11743098574253,
+    last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+});
+
+parkingRef.add({
+    code: "AdvancedParkingHowe9167",
+    name: "Advanced Parking Howe St - Lot #9167",
+    city: "Vancouver",
+    province: "BC",
+    address: "980 Howe Street, Vancouver BC V6Z 1N9",
+    hours_of_operation: "7am-7pm",
+    rate: "$5.50 per hour",
+    details: "This new underground parkade featuring 217 stalls under brand new office tower at 980 Howe Street is clean, bright, security patrolled, with large - regular and small car stalls - motorbike parking - EV stations plus handicap parking. This parkade is located near hotels and retail shopping centers",
+    lat: 49.28013108501068,
+    lng: -123.12324671093894,
+    last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+});
+
+parkingRef.add({
+    code: "PivotalBuilding1610",
+    name: "858 Beatty - Pivotal Building - Lot #1610",
+    city: "Vancouver",
+    province: "BC",
+    address: "858 Beatty St, Vancouver, BC V6B 1C1",
+    hours_of_operation: "7am-1am",
+    rate: "$5.00 per hour",
+    details: "This clean and brightly lit underground parking facility is located at the Pivotal Building on the east corner of Beatty Street and Smithe Street; close to BC Place Stadium and Yaletown. There are a wide variety of restaurants, hospitality services, shopping destinations and convenient access to transit available nearby.",
+    lat: 49.27740089865711,
+    lng: -123.11465106955497,
+    last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+});
+
+parkingRef.add({
+    code: "Diamond4264",
+    name: "Diamond Parking Lot 4264",
+    city: "Vancouver",
+    province: "BC",
+    address: "1022 Nelson St #506, Vancouver, BC V6E 4S7",
+    hours_of_operation: "24/7",
+    rate: "$9.00 per hour",
+    details: "Lot info: The parking lot is gated with access between 6AM - 11:30PM only. Clearance is 7 ft. This is a virtual permit; no physical permit is sent out.Once purchased please ensure your license plate of the vehicles parking are correctly inputted and registered to your account/ permit to avoid any parking notices.",
+    lat: 49.281869952658006,
+    lng: -123.1271649874657,
+    last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+});
+
+parkingRef.add({
+    code: "ParamountPlace9166",
+    name: "Paramount Place Parking Lot #9166",
+    city: "Vancouver",
+    province: "BC",
+    address: "900 Burrard St, Vancouver, BC V6Z 3G5",
+    hours_of_operation: "24/7",
+    rate: "$5.00 per hour +tax",
+    details: "This underground lot is located at Paramount Place, underneath the Scotiabank Theatre, on Burrard Street, between Barclay and Smithe Street.  Aside from serving as the primary parking location for visitors to the ScotiaBank Theatre, it is in close proximity to a variety of other destinations, including the Robson Street Shopping District, the Provincial Law Courts, Earls Restaurant, IGA Marketplace and the Sutton Place Hotel.",
+    lat: 49.282150151047,
+    lng: -123.12452188256384,
+    last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+});
+
+parkingRef.add({
+    code: "DavieStreet9027",
+    name: "1237 Davie Street - Lot #9027",
+    city: "Vancouver",
+    province: "BC",
+    address: "1237 Davie St, Vancouver, BC V6E 1N4",
+    hours_of_operation: "24/7",
+    rate: "$5.00 per hour",
+    details: "This underground parkade is located on Davie Street, between Bute and Jervis. There are a wide variety of restaurants, hotels, professional services, shopping destinations and convenient access to transit available nearby",
+    lat: 49.282479471471575,
+    lng: -123.13370656576048,
+    last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+});
+
+parkingRef.add({
+    code: "DenmanPendrell9175",
+    name: "Denman & Pendrell - Lot #9175",
+    city: "Vancouver",
+    province: "BC",
+    address: "1125 Denman Street, Vancouver BC V6G 2M7",
+    hours_of_operation: "24/7",
+    rate: "$5.50 per hour +tax",
+    details: "This surface parking lot is located at the heart of English Bay one block up from Davie Street. With easy access off Pendrell Street we are open 24/7 and within walking distance to unlimited entertainment and dining options. Residents and tourists alike are invited to park and enjoy this prominent location so close to the beach! Rental and filming requests are also welcome.",
+    lat: 49.288159118110286,
+    lng: -123.14075283215536,
+    last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+});
